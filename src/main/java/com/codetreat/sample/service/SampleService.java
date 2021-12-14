7a71d085f0802fb9e21278274312fb73c38bb111
Michele Mastroianni <22447148+mastroiannim@users.noreply.github.com>
2021-12-14 10:52:58 +0100

    ucanaccess attempt

--- src/main/java/com/codetreat/sample/service/SampleService.java
+++ src/main/java/com/codetreat/sample/service/SampleService.java
@@ -3,18 +3,26 @@ package com.codetreat.sample.service;
 import java.util.List;
  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
- 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.codetreat.sample.entity.SampleEntity;
 import com.codetreat.sample.repository.SampleRepository;
  
+@SpringBootApplication
 @Service
 public class SampleService {
  
     @Autowired
     SampleRepository sampleRepo;
- 
-        public List<SampleEntity> getAll() {
+    @RequestMapping(value = "/sample")
+    public List<SampleEntity> getAll() {
         return sampleRepo.getAll();
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleService.class, args);
+      }
 }
