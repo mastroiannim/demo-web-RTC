68cc08ae6a4515d17910860fdf1b97bc1e02bcb6
mastroiannim <22447148+mastroiannim@users.noreply.github.com>
2021-12-17 20:14:31 +0000

    single application refactoring

--- src/main/java/com/codetreat/sample/SampleApplication.java
+++ src/main/java/com/codetreat/sample/SampleApplication.java
@@ -1,32 +1,34 @@
 package com.codetreat.sample;
 
 
+import com.baeldung.SocketHandler;
 import com.codetreat.sample.controller.SampleController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
- 
+
+@Configuration
+@EnableWebSocket
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication implements WebSocketConfigurer{
 
   @Autowired
   SampleController controller;
 
-  
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+      registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(SampleApplication.class, args);
   }
 
-
-  @Bean
-  public ServletServerContainerFactoryBean createWebSocketContainer() {
-    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-    container.setMaxBinaryMessageBufferSize(1024000);
-    return container;
-  }
 
 }
